0bdd49962f715106da2f819401c7830050a990cc
Utsav <utsav.subedi55@gmail.com>
2024-06-20 10:21:05 +0545

    Add project files.

---
+++ CRUD-MVC-dotnet/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CRUD_MVC_dotnet.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_MVC_dotnet.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        { 
+
+        }
+
+        public DbSet<Student> Students {  get; set; } 
+
+
+    }
+}
